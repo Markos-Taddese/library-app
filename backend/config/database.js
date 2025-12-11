1d0149bddcb56e24d17f3896f99a068aa8bd0ccd
Markos <markostm02@gmail.com>
2025-12-11 10:17:58 -0800

    Fix: Add mandatory SSL/TLS config for TiDB

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,4 +1,5 @@
 const mysql=require('mysql2')
+const fs = require('fs');
 require('dotenv').config({path:'../.env'});
 // We use createPool (instead of createConnection) to efficiently manage multiple
 // simultaneous connections. This prevents resource exhaustion and improves
@@ -8,6 +9,11 @@ const pool=mysql.createPool({
     user:process.env.DB_USER,
     password:process.env.DB_PASSWORD,
     database:process.env.DB_DATABASE,
+    ssl: {
+        // connecting to TiDB Cloud,must use the server's certificate.
+        //set rejectUnauthorized to true to verify the server's identity.
+        rejectUnauthorized: true, 
+    },
     connectionLimit: parseInt(process.env.DB_CONNECTION_LIMIT) || 10,// Max number of simultaneous connections allowed
     waitForConnections:true,// Queue requests if the limit is reached
     queueLimit: parseInt(process.env.DB_QUEUE_LIMIT) || 10// Max number of requests allowed in the queue
